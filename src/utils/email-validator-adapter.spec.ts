b65e800b9ba0c612cb471182c937ff4ae3f32cec
Ana Mitsutani <ana.mitsutani@gmail.com>
2021-02-18 16:03:07 -0300

    test: enrsure EmailValidatorAdapter returns false if validator returns false

---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,9 @@
+import { EmailValidatorAdapter } from './email-validator'
+
+describe('EmailValidator Adaptor', () => {
+  test('Should return false if validator returns false', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('invalidemail@mail.com')
+    expect(isValid).toBe(false)
+  })
+})
