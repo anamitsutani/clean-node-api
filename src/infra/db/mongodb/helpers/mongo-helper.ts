3337d41c30f40786d37be605581dfbfe9c35c35c
Ana Mitsutani <ana.mitsutani@gmail.com>
2021-02-25 16:02:15 -0300

    refactor: move mapping to mongo helper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -17,5 +17,10 @@ export const MongoHelper = {
 
   getCollection (name: string): Collection {
     return this.client.db().collection(name)
+  },
+
+  map: (collection: any): any => {
+    const { _id, ...accountWithouId } = collection
+    return Object.assign({}, accountWithouId, { id: _id })
   }
 }
