9330ae629ffac41ff4fc20fc3e30244a2c34aff0
Ana Mitsutani <ana.mitsutani@gmail.com>
2021-03-31 11:42:46 -0300

    feat: ensure SignUpController returns 400 if ValidationStub fails

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -15,7 +15,10 @@ export class SignUpController implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      this.validation.validate(httpRequest.body)
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
       //  check if all fields provided
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
