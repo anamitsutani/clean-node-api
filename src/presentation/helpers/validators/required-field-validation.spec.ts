54e91ee13cd37e81b308f3308e10cba9eedcb117
Ana Mitsutani <ana.mitsutani@gmail.com>
2021-04-01 09:51:45 -0300

    test: ensure RequiredFiledValidation not return if validation succeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -2,9 +2,15 @@ import { MissingParamError } from '../../errors'
 import { RequiredFieldValidation } from './required-field-validation'
 
 describe('Email Validation', () => {
-  test('Should return a MissingParamError if Validation fails', () => {
+  test('Should return a MissingParamError if validation fails', () => {
     const sut = new RequiredFieldValidation('anyfield')
     const error = sut.validate({ name: 'anyname' })
     expect(error).toEqual(new MissingParamError('anyfield'))
+  })
+
+  test('Should not return if validation succeds', () => {
+    const sut = new RequiredFieldValidation('anyfield')
+    const error = sut.validate({ anyfield: 'anyvalue' })
+    expect(error).toBeFalsy()
   })
 })
