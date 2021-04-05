aa6eff77d2db52be9f2af04c7def7d5a18d7e5a3
Ana Mitsutani <ana.mitsutani@gmail.com>
2021-04-05 16:30:22 -0300

    refactor: remove duplicate code

--- src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
@@ -3,11 +3,7 @@ import { Hasher } from '../../../data/protocols/criptography/hasher'
 
 // the salt property is specific from bcrypt so we cant put it in the protocol, we have to add it here in the adapter
 export class BcryptAdapter implements Hasher {
-  private readonly salt
-
-  constructor (salt: number) {
-    this.salt = salt
-  }
+  constructor (private readonly salt: number) { }
 
   async hash (value: string): Promise<string> {
     const hash = await bcrypt.hash(value, this.salt)
