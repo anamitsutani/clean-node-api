3f1057d0a5d71cde527a9a2711e5b1f8761cc23c
Ana Mitsutani <ana.mitsutani@gmail.com>
2021-04-12 13:33:52 -0300

    refactor: separate factories in folders

--- src/main/factories/controllers/signup/signup-validation-factory.ts
+++ src/main/factories/controllers/signup/signup-validation-factory.ts
@@ -1,7 +1,7 @@
-import { ValidationComposite, RequiredFieldValidation, CompareFieldsValidation } from '../../../presentation/helpers/validators'
-import { Validation } from '../../../presentation/protocols/validation'
-import { EmailValidatorAdapter } from '../../adapters/validators/email-validator-adapter'
-import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
+import { ValidationComposite, RequiredFieldValidation, CompareFieldsValidation } from '../../../../presentation/helpers/validators'
+import { Validation } from '../../../../presentation/protocols/validation'
+import { EmailValidatorAdapter } from '../../../adapters/validators/email-validator-adapter'
+import { EmailValidation } from '../../../../presentation/helpers/validators/email-validation'
 
 export const makeSignUpValidation = (): ValidationComposite => {
   const validations: Validation[] = []
