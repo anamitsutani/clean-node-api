ce4264d9783dcc5111502cd281a9f58a8b4f7daa
Ana Mitsutani <ana.mitsutani@gmail.com>
2021-05-05 19:28:18 -0300

    test: ensure add survey routes return 204 if no valid token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,8 +2,11 @@ import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
 let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Surveys Routes', () => {
   beforeAll(async () => {
@@ -17,6 +20,8 @@ describe('Surveys Routes', () => {
   beforeEach(async () => {
    surveyCollection = await MongoHelper.getCollection( 'surveys')
     await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   describe('POST/surveys', () => {
@@ -33,6 +38,37 @@ describe('Surveys Routes', () => {
           }]
         })
         .expect(403)
+    })
+
+    test('Should return 204 on add survey wth valid acessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'ana',
+        email: 'ana@gmail.com',
+        password: '123',
+        role: 'admin'
+      })
+      const id = res.ops[0]._id
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'anyquestion',
+          answers: [{
+            image: 'http://image-name.com',
+            answer: 'anyanswer'
+          }, {
+            answer: 'otheranswer'
+          }]
+        })
+        .expect(204)
     })
   })
 })
